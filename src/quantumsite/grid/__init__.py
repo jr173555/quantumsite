c2555f15fefdf42e342243ce8f436589f4192902
jr173555 <annika.hartz@rwth-aachen.de>
2026-05-24 15:00:07 +0200

    edges use vertices instead of coords

--- src/quantumsite/grid/__init__.py
+++ src/quantumsite/grid/__init__.py
@@ -5,12 +5,20 @@ for y in range(6):  # 0 to 5
         coords.append((x, y, 0))
 
 
+
 edges = []
 for y in range(6):
     for x in range(6):
+        coord1 = (x, y , 0)
+        idx1 = coords.index(coord1)
         if x < 5:
-            edges.append(((x, y, 0), (x+1, y, 0)))  # connect to right neighbor
+            coord2 = (x + 1, y, 0)
+            idx2 = coords.index(coord2)
+            edges.append((idx1, idx2))  # connect to right neighbor
         if y < 5:
-            edges.append(((x, y, 0), (x, y+1, 0)))  # connect to upper neighbor
+            coord3 = (x, y + 1, 0)
+            idx3 = coords.index(coord3)
+            edges.append((idx1, idx3))  # connect to upper neighbor
 
-print (coords, edges)
+print(coords)
+print(edges)
