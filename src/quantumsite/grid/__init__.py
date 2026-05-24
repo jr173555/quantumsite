76cc18f2593678a291fbf587a25e82d71c59f6c8
jr173555 <annika.hartz@rwth-aachen.de>
2026-05-24 15:30:24 +0200

    function for user input of grid size

--- src/quantumsite/grid/__init__.py
+++ src/quantumsite/grid/__init__.py
@@ -1,24 +1,25 @@
-coords = []
-for y in range(6):  # 0 to 5
-    for x in range(6):  # 0 to 5
-        #for z in range(6):  # 0 to 5
-        coords.append((x, y, 0))
+def unspec_grid(n):
+    coords = []
+    for y in range(n):  # 0 to 5
+        for x in range(n):  # 0 to 5
+            #for z in range(6):  # 0 to 5
+            coords.append((x, y, 0))
 
 
 
-edges = []
-for y in range(6):
-    for x in range(6):
-        coord1 = (x, y , 0)
-        idx1 = coords.index(coord1)
-        if x < 5:
-            coord2 = (x + 1, y, 0)
-            idx2 = coords.index(coord2)
-            edges.append((idx1, idx2))  # connect to right neighbor
-        if y < 5:
-            coord3 = (x, y + 1, 0)
-            idx3 = coords.index(coord3)
-            edges.append((idx1, idx3))  # connect to upper neighbor
+    edges = []
+    for y in range(n):
+        for x in range(n):
+            coord1 = (x, y , 0)
+            idx1 = coords.index(coord1)
+            if x < n-1:
+                coord2 = (x + 1, y, 0)
+                idx2 = coords.index(coord2)
+                edges.append((idx1, idx2))  
+            if y < n-1:
+                coord3 = (x, y + 1, 0)
+                idx3 = coords.index(coord3)
+                edges.append((idx1, idx3))
+    
+    return coords, edges
 
-print(coords)
-print(edges)
