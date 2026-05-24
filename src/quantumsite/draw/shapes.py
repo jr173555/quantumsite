5817d09406f220c94d4ee611d4468a09efbf5be5
diazlcarlos98 <diazlcarlos98@gmail.com>
2026-05-24 16:29:44 +0200

    fix: box as squares

--- src/quantumsite/draw/shapes.py
+++ src/quantumsite/draw/shapes.py
@@ -4,15 +4,20 @@ import plotly.express as px
 
 cmap = px.colors.sequential.Agsunset
 
+
 class Canvas:
 
     def __init__(
-        self, coordinates: list[tuple[float, float, float]],
+        self,
+        coordinates: list[tuple[float, float, float]],
         edges: list[tuple[int, int]],
     ):
         self.coordinates: list[tuple[float, float, float]] = coordinates
         self.edges: list[tuple[int, int]] = edges
-        self.height_lims = np.min(coordinates, axis=0)[2], np.max(coordinates, axis=0)[2]
+        self.height_lims = (
+            np.min(coordinates, axis=0)[2],
+            np.max(coordinates, axis=0)[2],
+        )
 
     def draw(self):
         figure = go.Figure()
@@ -21,7 +26,7 @@ class Canvas:
             box = Box(idx, x, y, z)
             box.draw_box(figure, self.height_lims)
 
-        #for idx1, idx2 in self.edges:
+        # for idx1, idx2 in self.edges:
         #    x1, y1, _ = self.coordinates[idx1]
         #    x2, y2, _ = self.coordinates[idx2]
         #    figure.add_trace(
@@ -38,22 +43,26 @@ class Canvas:
             title="Quantum Site Visualization",
             xaxis_title="X Coordinate",
             yaxis_title="Y Coordinate",
+            xaxis=dict(constrain="domain", showgrid=False),
+            yaxis=dict(scaleanchor="x", scaleratio=1, showgrid=False),
             showlegend=True,
         )
         figure.show()
-        
+
 
 class Box:
 
-    def __init__(self, idx: int, x: float, y: float, z: float, size: float = 5):
+    def __init__(self, idx: int, x: float, y: float, z: float, size: float = 10):
         self.idx: int = idx
         self.x: float = x
         self.y: float = y
         self.z: float = z
         self.size: float = size
 
-    def draw_box(self, figure: go.Figure, height_lims: None | tuple[float, float] = None):
-        if height_lims is not None:
+    def draw_box(
+        self, figure: go.Figure, height_lims: None | tuple[float, float] = None
+    ):
+        if height_lims is not None and (height_lims[0] < height_lims[1]):
             min_height, max_height = height_lims
             relative_z = (self.z - min_height) / (max_height - min_height)
             color = cmap[int(relative_z * (len(cmap) - 1))]
@@ -62,7 +71,7 @@ class Box:
             color = "lightgray"
             text = f"Index: {self.idx}"
 
-        marker = {"size": 15, "color": color, "symbol": "circle"}
+        marker = {"size": 15, "color": color, "symbol": "square"}
         figure.add_trace(
             go.Scatter(
                 x=[self.x],
